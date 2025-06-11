d1a64e465bb92e30f0e6e2ed0683f79be57aba1c
Suman Srinivas <sumansrinivas@Sumans-MacBook-Pro.local>
2025-06-11 10:38:03 +0530

    updated one

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 const methodOverride = require('method-override');
 const { v4: uuidv4 } = require('uuid');
-const { rmSync } = require('fs');
+
 
 app.use(methodOverride('_method'));
 app.use(express.urlencoded({extended: true}));
